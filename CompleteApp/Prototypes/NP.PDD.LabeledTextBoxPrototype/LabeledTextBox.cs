28d408f5e7996c3c79e07e95268c346ae2ebd40d
npolyak <nick.polyak@verizon.net>
2022-02-03 11:53:39 -0500

    after step1

---
+++ CompleteApp/Prototypes/NP.PDD.LabeledTextBoxPrototype/LabeledTextBox.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace NP.PDD.LabeledTextBoxPrototype
+{
+    public class LabeledTextBox : TemplatedControl
+    {
+
+        #region Label Styled Avalonia Property
+        public string Label
+        {
+            get { return GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public static readonly StyledProperty<string> LabelProperty =
+            AvaloniaProperty.Register<LabeledTextBox, string>
+            (
+                nameof(Label)
+            );
+        #endregion Label Styled Avalonia Property
+
+
+        #region Text Styled Avalonia Property
+        public string Text
+        {
+            get { return GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly StyledProperty<string> TextProperty =
+            AvaloniaProperty.Register<LabeledTextBox, string>
+            (
+                nameof(Text)
+            );
+        #endregion Text Styled Avalonia Property
+    }
+}
